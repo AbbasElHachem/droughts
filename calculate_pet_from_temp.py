9288d5eedde319bdd87d228a86980f41030b9f91
hachem <hachem@lhg-102.iws.uni-stuttgart.de>
2019-10-22 08:51:22 +0200

    ds

--- calculate_pet_from_temp.py
+++ calculate_pet_from_temp.py
@@ -1,49 +1,80 @@
 import os
 
-import class_calculate_PET
-import class_calculate_SPI
+
 import pandas as pd
 
 
-sss = class_calculate_PET.Calculate_PET()
+def hargreaves_pet(d_o_y, lat_, t_min, t_max, t_avg):
+    """
+    Purpose: To get the potential evapotranspiration at a given latitude \
+                for a given date and temperature.
+    Description of the arguments:
+        d_o_y (int): day of the year.
+        lat (degrees): latitude of the point.
+        t_min (celsius): minimum temperature on that day
+        t_max (celsius): maximum temperature on that day
+        t_avg (celsius): average temperature on that day
+    """
+    from numpy import radians, sin, pi
+    from math import acos, tan, cos, sqrt
+    tot_days = 365
+    lat = radians(lat_)
+    ndec = 0.409 * sin(((2 * pi * d_o_y) / tot_days) - 1.39)
 
-main_dir = os.path.join(r'X:\hiwi\ElHachem\AdvancedPython\SPI_droughts')
+    nws = acos(-tan(lat) * tan(ndec))
+    dfr = 1 + (0.033 * cos((2 * pi * d_o_y) / tot_days))
+    fac_1 = 15.342618001389575  # ((1440 * 0.082 * 0.4082)/pi)
+    ra = fac_1 * dfr * ((nws * sin(lat) * sin(ndec)) +
+                        (cos(lat) * cos(ndec) * sin(nws)))
+    fac_2 = 0.002295  # (0.0135 * 0.17)
+    pet = fac_2 * ra * sqrt(t_max - t_min) * (t_avg + 17.8)
+
+    if pet > 0:
+        return pet
+    else:
+        return 0
+
+
+main_dir = os.path.join(r'C:\Users\hachem\Desktop\data_Gaza')
 #main_dir = class_calculate_SPI.set_main_dir(main_dir)
 
-in_temp_file = os.path.join(main_dir,
-                            r'data\SA_temp_data_1901_2015.csv')
+
 in_temp_min_file = os.path.join(main_dir,
-                                r'data\SA_temp_min_var_.csv')
+                                r'tmin\gaza_temp_min_var_.csv')
 in_temp_max_file = os.path.join(main_dir,
-                                r'data\SA_temp_max_var_.csv')
-in_ppt_file = os.path.join(main_dir,
-                           r'data\SA_pr_1901_2015.csv')
+                                r'tmax\gaza_temp_max_var_.csv')
 
-in_ppt_df = sss.read_df(in_ppt_file, 0, ';', '%Y-%m-%d')
-in_temp_df = sss.read_df(in_temp_file, 0, ';', '%Y-%m-%d')
-in_temp_df_min = sss.read_df(in_temp_min_file, 0, ';', '%Y-%m-%d')
-in_temp_df_max = sss.read_df(in_temp_max_file, 0, ';', '%Y-%m-%d')
-in_temp_df_min.dropna(inplace=True)
-in_temp_df_max.dropna(inplace=True)
 
+# read df min
+in_temp_df_min = pd.read_csv(in_temp_min_file, index_col=0,
+                             sep=';', parse_dates=True,
+                             infer_datetime_format=True)
+
+# read df max
+in_temp_df_max = pd.read_csv(in_temp_max_file, index_col=0,
+                             sep=';', parse_dates=True,
+                             infer_datetime_format=True)
+# calculate df avg
 mean_temp = (in_temp_df_max.values + in_temp_df_min.values) / 2
+# make it a dataframe
 mean_temp_Df = pd.DataFrame(index=in_temp_df_max.index, data=mean_temp,
                             columns=['avg'])
 
-pet = [sss.hargreaves_pet(doy, -26.25, tmins, tmaxs, tmean)
-       for doy, tmean, tmaxs, tmins in zip(in_temp_df_min.index.dayofyear,
+pet = [hargreaves_pet(doy, 31.443895, tmins, tmaxs, t_avg)
+       for doy, t_avg, tmaxs, tmins in zip(in_temp_df_min.index.dayofyear,
                                            in_temp_df_min.values,
                                            in_temp_df_max.values,
                                            mean_temp_Df.values)]
 df_pet = pd.DataFrame(index=in_temp_df_min.index, data=pet)
-df_mtl = df_pet.resample('M', label='right', closed='right').sum()
+
+# df_mtl = df_pet.resample('M', label='right', closed='right').sum()
 
 
-ppt_cmn_idx = in_ppt_df.index.intersection(df_mtl.index)
-spdi_vals = in_ppt_df.loc[ppt_cmn_idx, :].values - \
-    df_mtl.loc[ppt_cmn_idx, :].values
-spdi_df = pd.DataFrame(index=ppt_cmn_idx, data=spdi_vals)
+# ppt_cmn_idx = in_ppt_df.index.intersection(df_mtl.index)
+# spdi_vals = in_ppt_df.loc[ppt_cmn_idx, :].values - \
+#     df_mtl.loc[ppt_cmn_idx, :].values
+# spdi_df = pd.DataFrame(index=ppt_cmn_idx, data=spdi_vals)
 
-df_mtl.to_csv(os.path.join(main_dir, 'data\SA_pet_var.csv'), sep=';')
+df_pet.to_csv(os.path.join(main_dir, 'gaza_pet_var.csv'), sep=';')
 
-spdi_df.to_csv(os.path.join(main_dir, 'data\SA_ppt_min_pet_var.csv'), sep=';')
+# spdi_df.to_csv(os.path.join(main_dir, 'data\SA_ppt_min_pet_var.csv'), sep=';')
--- extract_gpcp_ppt_data.py
+++ extract_gpcp_ppt_data.py
@@ -17,8 +17,10 @@ import pandas as pd
 print('\a\a\a\a Started on %s \a\a\a\a\n' % time.asctime())
 START = timeit.default_timer()  # to get the runtime of the program
 
-main_dir = os.path.join(r'X:\hiwi\ElHachem\AdvancedPython\SPI_droughts\data')
+main_dir = os.path.join(r'C:\Users\hachem\Desktop\data_Gaza\tmin')
 os.chdir(main_dir)
+
+variable_name = 'tmin'  # tmax
 
 
 def getFiles(data_dir_, file_ext_str):
@@ -41,24 +43,22 @@ def resampleDf(data_frame, temp_freq):
 
 
 in_nc_files = getFiles(
-    r'C:\Users\hachem\Desktop\full_data_monthly_v2018_025.nc',
+    main_dir,
     '.nc')
 assert len(in_nc_files) > 0, 'no nc files found'
-wanted_date_fmt = '%Y-%m-%d  %H:%M:%S'
-in_time_name = 'time'
-x_coord = 233  # idx=224 lat=34.131, value for Lebanon
-y_coord = 57  # idx=864 lon=35.90, value for Lebanon
 
-xMin, yMin = 25.2356, -26.1941
-xMax, yMax = 33.9993, -19.7914
-# subset = ppt_vals.isel(0, lat=slice(439, 466), lon=slice(821, 857)).values
+wanted_date_fmt = '%Y-%m-%d'
+
+in_time_name = 'time'
+
+x_coord_1 = 69
+y_coord_1 = 117
+x_coord_2 = 70
+y_coord_2 = 118
+
+
 idx_ = []
 vals = []
-# time_idx = pd.date_range(
-#     start='1979',
-#     end='22-08-2018',
-#     freq='D',
-#     closed='left')
 
 for nc_f in in_nc_files:
     in_nc = nc.Dataset(nc_f)
@@ -67,34 +67,33 @@ for nc_f in in_nc_files:
 #     # convert time array
     time_arr = nc.num2date(time_var[:],
                            time_var.units,
-                           calendar=time_var.calendar)
+                           calendar='standard')
 #                           calendar='standard')
 
-    # get the various variables as arrays
-#     lat_arr = in_nc.variables['lat'][:]
-#     lon_arr = in_nc.variables['lon'][:]
-
     # read and make time for pandas datetime
-
-    #
-    # select ppt data
-    ppt_vals = in_nc.variables['tmax']
+    # select temp data
+    ppt_vals = in_nc.variables[variable_name][:].data  # change tmax, tmin
 
     for idx, date in enumerate(time_arr):
 
-        all_ppt_vals = ppt_vals[idx, x_coord, y_coord]
+        all_ppt_vals_1 = ppt_vals[idx, y_coord_1, x_coord_1]
+        all_ppt_vals_2 = ppt_vals[idx, y_coord_2, x_coord_2]
+
+        all_ppt_vals = (all_ppt_vals_1 + all_ppt_vals_2) / 2
         vals.append(all_ppt_vals)
         idx_.append(date)
+
     in_nc.close()
 
 df_ = pd.DataFrame(data=vals, dtype=float)
 str_time = [i.strftime(wanted_date_fmt) for i in idx_]
 time_idx = pd.DatetimeIndex(pd.to_datetime(str_time, format=wanted_date_fmt))
-df_.columns = ['tmax']
+df_.columns = [variable_name]  # change tmax, tmin
 df_.loc[:, 'time'] = time_idx
 df_.set_index('time', drop=True, inplace=True)
 # df_monthly = resampleDf(df_, 'M')
-df_.to_csv(os.path.join(main_dir, 'SA_temp_max_var_.csv'), sep=';')
+df_.to_csv(os.path.join(main_dir, 'gaza_temp_%s_var_.csv' %
+                        variable_name), sep=';')
 
 STOP = timeit.default_timer()  # Ending time
 print(('\n\a\a\a Done with everything on %s. Total run time was'
